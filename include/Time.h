44b5087ab0dbf499eb90e7701765c983631219b4
Gri1915 <psihamaks@gmail.com>
2021-12-12 23:51:08 +0300

    add include

---
+++ include/Time.h
@@ -0,0 +1,25 @@
+//Copyright by Gri1915
+
+#ifndef INCLUDE_TIME_HPP_
+#define INCLUDE_TIME_HPP_
+
+#include <iostream>
+#include <random>
+#include <vector>
+
+#include "Check.h"
+
+using std::string;
+
+using std::vector;
+
+struct Time {
+ public:
+  void print(std::ostream& os);
+  Check_type type;
+  std::vector<Check> experiments;
+  Time(Check_type type, std::vector<long>& sizes);
+};
+
+
+#endif // INCLUDE_TIME_HPP_
